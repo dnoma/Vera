dc239068381d06a4201cac304582a71174ad2ea1
digitalnoma <marcusng0104@outlook.com>
2026-01-18 18:38:43 +0800

    chore: add public API entrypoint

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+/**
+ * Vera public API.
+ *
+ * Re-exports core types and modules plus semantics/contestation utilities.
+ */
+
+export * from './core/types.js';
+export * from './core/Claim.js';
+export * from './core/Source.js';
+export * from './core/Argument.js';
+export * from './core/Relation.js';
+export * from './core/Uncertainty.js';
+export * from './core/Decision.js';
+export * from './core/ArgumentationFramework.js';
+
+export * from './integrity/index.js';
+export * from './semantics/index.js';
+export * from './contestation/index.js';
+
